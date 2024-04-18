d68999d542ac7aa262e1f4f00c6583160e614208
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2024-04-18 22:36:09 +0800

    Feat: crud movie (#2)

---
+++ database/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+
+// Get all movies
+app.get('/movies', async (req, res) => {
+  const movies = await prisma.movie.findMany();
+  res.json(movies);
+});
+
+// Get a movie by id
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const movie = await prisma.movie.findUnique({
+    where: { id: parseInt(id) },
+  });
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).send('Movie not found');
+  }
+});
+
+// Create a new movie
+app.post('/movies', async (req, res) => {
+  const { title, director, releaseDate, genre, duration, rating } = req.body;
+  const movie = await prisma.movie.create({
+    data: { title, director, releaseDate, genre, duration, rating },
+  });
+  res.status(201).json(movie);
+});
+
+// Update a movie by id
+app.put('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, director, releaseDate, genre, duration, rating } = req.body;
+  try {
+    const updatedMovie = await prisma.movie.update({
+      where: { id: parseInt(id) },
+      data: { title, director, releaseDate, genre, duration, rating },
+    });
+    res.json(updatedMovie);
+  } catch (error) {
+    res.status(404).send('Movie not found');
+  }
+});
+
+// Delete a movie by id
+app.delete('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await prisma.movie.delete({
+      where: { id: parseInt(id) },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(404).send('Movie not found');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
