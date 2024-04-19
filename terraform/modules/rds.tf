e48c4617267b3b89c554aa76a65eec98b773fbd7
franky-lim24 <franky.2021@scis.smu.edu.sg>
2024-04-19 18:13:57 +0800

    fix: rds issue

--- terraform/modules/rds.tf
+++ terraform/modules/rds.tf
@@ -1,4 +1,5 @@
 resource "aws_db_instance" "backend_db" {
+  identifier = "cinemate-db"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
@@ -10,6 +11,7 @@ resource "aws_db_instance" "backend_db" {
   parameter_group_name = "default.mysql8.0"
   db_subnet_group_name = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.db.id]
+  skip_final_snapshot = true
 
   tags = {
     Name = "cinemate"
