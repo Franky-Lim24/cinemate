f2c4fd5385133d3571cfbf3d81b5236d63030c11
franky-lim24 <franky.2021@scis.smu.edu.sg>
2024-04-20 00:43:26 +0800

    fix: add health check

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Health check
+app.get('/', (req, res) => {
+  res.send('Welcome to Cinemate API');
+});
+
 // Get all movies
 app.get('/api/movies', async (req, res) => {
   const movies = await prisma.movie.findMany();
