cadd0b0c825b0f597c0ccfc1b74a84e0d71a847b
franky-lim24 <franky.2021@scis.smu.edu.sg>
2024-04-20 01:53:41 +0800

    test: check hello world

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
 // Health check
 app.get('/api/health', (req, res) => {
   res.status(200).send('Welcome to Cinemate API');
