eb5396f39d86f3cc85f1956ca4d2fbf95f741980
franky-lim24 <franky.2021@scis.smu.edu.sg>
2024-04-19 19:16:34 +0800

    fix: overlapping CIDR

--- terraform/modules/network.tf
+++ terraform/modules/network.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "public1" {
 
 resource "aws_subnet" "public2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "ap-southeast-1b"
   map_public_ip_on_launch = true
 }
