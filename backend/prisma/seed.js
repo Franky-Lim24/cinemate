63143a527c46c7efa4d41cb3494971ec6cfd8c5e
franky-lim24 <franky.2021@scis.smu.edu.sg>
2024-04-19 23:28:01 +0800

    fix: migration and seed

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -7,7 +7,7 @@ async function main() {
       id: 1,
       title: 'Pirates of the Caribbean',
       director: 'Espen Sandberg',
-      imageUrl:
+      image:
         'https://m.media-amazon.com/images/M/MV5BMjE5MjkwODI3Nl5BMl5BanBnXkFtZTcwNjcwMDk4NA@@._V1_.jpg',
       releaseYear: 2003,
     },
@@ -15,7 +15,7 @@ async function main() {
       id: 2,
       title: 'Inception 2231',
       director: 'Christopher Nolan',
-      imageUrl:
+      image:
         'https://m.media-amazon.com/images/M/MV5BMjExMjkwNTQ0Nl5BMl5BanBnXkFtZTcwNTY0OTk1Mw@@._V1_.jpg',
       releaseYear: 2010,
     },
@@ -23,15 +23,15 @@ async function main() {
       id: 3,
       title: 'Interstellar',
       director: 'Christopher Nolan',
-      imageUrl:
+      image:
         'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
       releaseYear: 2014,
     },
     {
       id: 4,
       title: 'The Matrix',
-      directors: 'Lana Wachowski, Lilly Wachowski',
-      imageUrl:
+      director: 'Lana Wachowski, Lilly Wachowski',
+      image:
         'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
       releaseYear: 1999,
     },
@@ -39,7 +39,7 @@ async function main() {
       id: 5,
       title: 'Pulp Fiction',
       director: 'Quentin Tarantino',
-      imageUrl:
+      image:
         'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
       releaseYear: 1994,
     },
@@ -47,7 +47,7 @@ async function main() {
       id: 6,
       title: 'The Godfather',
       director: 'Francis Ford Coppola',
-      imageUrl:
+      image:
         'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
       releaseYear: 1972,
     },
@@ -55,7 +55,7 @@ async function main() {
       id: 7,
       title: 'Fight Club',
       director: 'David Fincher',
-      imageUrl:
+      image:
         'https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
       releaseYear: 1999,
     },
@@ -63,7 +63,7 @@ async function main() {
       id: 8,
       title: 'Forrest Gump',
       director: 'Robert Zemeckis',
-      imageUrl:
+      image:
         'https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg',
       releaseYear: 1994,
     },
@@ -71,14 +71,18 @@ async function main() {
       id: 9,
       title: 'The Dark Knight',
       director: 'Christopher Nolan',
-      imageUrl:
+      image:
         'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg',
       releaseYear: 2008,
     },
   ];
 
   for (let movie of movies) {
-    await prisma.movie.create({ data: movie });
+    try {
+      await prisma.movie.create({ data: movie });
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 
